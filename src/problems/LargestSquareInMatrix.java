8f06a8a9c0b46daf793a80de8f0ab31c9268623c
harish <icon.harish@gmail.com>
2021-03-15 22:13:10 +0530

    Binary search in loop and Largest square problem started

---
+++ src/problems/LargestSquareInMatrix.java
@@ -0,0 +1,18 @@
+package problems;
+
+public class LargestSquareInMatrix {
+	
+	public static void main(String[] args) {
+		int size = 4;
+		int[][] matrix = new int[size][size];
+		
+		LargestSquareInMatrix obj = new LargestSquareInMatrix();
+		System.out.println(obj.getSizeOfSquare(matrix));
+		
+	}
+	
+	private int getSizeOfSquare(int[][] matrix) {
+		return 0;
+	}
+
+}
