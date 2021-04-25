f2f2066011f13619df825343f3f934d7ced49ca9
harish <icon.harish@gmail.com>
2021-04-25 23:24:42 +0530

    Dynamic Programming Fibonacci

---
+++ src/problems/dynamicp/Fibonacci.java
@@ -0,0 +1,26 @@
+package problems.dynamicp;
+
+public class Fibonacci {
+	
+	final int MAX = 1000;
+	int[] cache = new int[MAX];
+	
+	public static void main(String[] args) {
+		System.out.println(new Fibonacci().fib(400));
+	}
+	
+	int fib (int n) {
+		
+		if (n <= 1) {
+			return n;
+		}
+		
+		if (cache[n] == 0) {
+			cache[n] = fib(n-1) + fib(n-2);
+		}
+		
+		return cache[n];
+		
+	}
+
+}
